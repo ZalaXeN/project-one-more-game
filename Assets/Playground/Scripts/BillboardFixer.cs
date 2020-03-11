e527670bb1933ce5fe412ff522df3c2bf147c142
ZalaXeN <ZalaXeN@users.noreply.github.com>
2020-03-11 22:55:27 +0700

    Add Test Billboard Script
    - Test Billboard
    - Test Camera

---
+++ Assets/Playground/Scripts/BillboardFixer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class BillboardFixer : MonoBehaviour
+{
+    MeshRenderer mesh;
+
+    void BillboardFix(Mesh mesh)
+    {
+        var vertices = mesh.vertices;
+        var uvs = new List<Vector3>(mesh.vertexCount);
+        for (var i = 0; i < mesh.vertexCount; ++i)
+        {
+            var uv = vertices[i];
+            uvs.Add(uv);
+        }
+        mesh.SetUVs(1, uvs);
+    }
+}
---
+++ Assets/Playground/Scripts/BillboardSprite.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BillboardSprite : MonoBehaviour
+{
+    private Camera _camera;
+
+    private void LateUpdate()
+    {
+        Billboarding();
+    }
+
+    private void Billboarding()
+    {
+        if(_camera == null)
+            _camera = Camera.main;
+
+        transform.LookAt(transform.position + _camera.transform.rotation * Vector3.forward,
+            _camera.transform.rotation * Vector3.up);
+
+        //transform.forward = _camera.transform.forward;
+    }
+}
