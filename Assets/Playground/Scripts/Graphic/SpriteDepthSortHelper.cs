8a098348c69a46f07002a62c24d265132ca6a85c
ZalaXeN <ZalaXeN@users.noreply.github.com>
2020-04-22 18:28:24 +0700

    Fix Sprite diffuse flicker Bug
    - use Transparent Queue on shader instead Opaque (Z -> Sorting Layer)
    - Add Sprite Depth Sorting Helper that sort sprite with Z-Order

---
+++ Assets/Playground/Scripts/Graphic/SpriteDepthSortHelper.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode()]
+[RequireComponent(typeof(Renderer))]
+public class SpriteDepthSortHelper : MonoBehaviour
+{
+    private const int SORTING_BASE = 100;
+    private const float UPDATE_TIME = 0.1f;
+
+    private float _updateTimer = 0f;
+    private Renderer _renderer;
+
+    public Transform target;
+
+    [Tooltip("Use this to offset the object slightly in front or behind the Target object")]
+    public int targetOffset = 0;
+
+    private void Reset()
+    {
+        UpdateTargetWithParent();
+        UpdateOffsetWithSortingOrder();
+    }
+
+    void LateUpdate()
+    {
+        UpdateSortingOrder();
+    }
+
+    void UpdateTargetWithParent()
+    {
+        target = transform.root;
+    }
+
+    void UpdateOffsetWithSortingOrder()
+    {
+        if (_renderer == null)
+            _renderer = GetComponent<Renderer>();
+
+        targetOffset = _renderer.sortingOrder;
+    }
+
+    void UpdateSortingOrder()
+    {
+        _updateTimer -= Time.deltaTime;
+        if (_updateTimer > 0f)
+            return;
+
+        _updateTimer = UPDATE_TIME;
+
+        if (target == null)
+            target = transform;
+
+        if(_renderer == null)
+            _renderer = GetComponent<Renderer>();
+
+        _renderer.sortingOrder = -(int)(target.position.z * SORTING_BASE) + targetOffset;
+    }
+}
