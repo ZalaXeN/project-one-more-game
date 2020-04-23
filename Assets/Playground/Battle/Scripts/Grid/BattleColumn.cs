733a6bdd31904265fa4e5e5dea91ca18f9e2c45a
ZalaXeN <ZalaXeN@users.noreply.github.com>
2020-04-23 18:29:22 +0700

    Add Battle Column

--- Assets/Playground/Battle/Scripts/Grid/BattleColumn.cs
+++ Assets/Playground/Battle/Scripts/Grid/BattleColumn.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 namespace ProjectOneMore.Battle
 {
@@ -10,30 +14,118 @@ namespace ProjectOneMore.Battle
         Right
     }
 
+#if UNITY_EDITOR
+    [ExecuteInEditMode]
+#endif
     public class BattleColumn : MonoBehaviour
     {
         public BattleSide side;
         public int columnNumber;
 
-        [Range(0, 4)]
+        public float zFront = -1f;
+        public float zBack = 3f;
+
+        public float paddingFront = 0.5f;
+        public float paddingBack = 1f;
+
+        [Range(0, 10)]
         public int unitNumber;
 
-        private static float zFront = -1f;
-        private static float zBack = 3f;
-        private static float thickness = 10f;
+        private static float _thickness = 10f;
+        private static float _rowHeight = 2f;
 
-        private Vector3 startLine = new Vector3();
-        private Vector3 endLine = new Vector3();
+        private bool _isRowsUpdating;
+        private List<float> _centeredAlignRowList = new List<float>();
+        private List<float> _rowPercentPosList = new List<float>();
 
+        private Vector3 _startLine = new Vector3();
+        private Vector3 _endLine = new Vector3();
+
+        private void Update()
+        {
+            UpdateRows();
+        }
+
+        private void UpdateRows()
+        {
+            if (_centeredAlignRowList.Count == unitNumber || unitNumber <= 0 || _isRowsUpdating)
+                return;
+
+            _isRowsUpdating = true;
+
+            // Divider Example
+            // 1 = 3
+            // 2 = 4
+            // 3 = 3
+            // 4 = 4...
+            int divider = unitNumber <= 2 ? unitNumber + 1 : unitNumber - 1;
+            float dividerRatio = 1f / divider;
+
+            _centeredAlignRowList.Clear();
+            _rowPercentPosList.Clear();
+            float cumulative = 0f;
+
+            for(int i = 0; i <= divider; i++)
+            {
+                _rowPercentPosList.Add(cumulative);
+                cumulative += dividerRatio;
+            }
+
+            int j = _rowPercentPosList.Count;
+            for (int i = 0; i < j; i++)
+            {
+                _centeredAlignRowList.Add(_rowPercentPosList[_rowPercentPosList.Count / 2]);
+                _rowPercentPosList.RemoveAt(_rowPercentPosList.Count / 2);
+            }
+
+            // CheckCenteredAlignRowList();
+
+            _isRowsUpdating = false;
+        }
+
+        //private void CheckCenteredAlignRowList()
+        //{
+        //    string resultCheck = "";
+        //    foreach (float result in _centeredAlignRowList)
+        //    {
+        //        resultCheck += result + ", ";
+        //    }
+        //    Debug.Log(resultCheck);
+        //}
+
+#if UNITY_EDITOR
         private void OnDrawGizmos()
         {
-            startLine = transform.position;
-            startLine.z = zFront;
+            _startLine = transform.position;
+            _startLine.z = zFront;
 
-            endLine = transform.position;
-            endLine.z = zBack;
+            _endLine = transform.position;
+            _endLine.z = zBack;
 
-            Handles.DrawBezier(startLine, endLine, startLine, endLine, Color.green, null, thickness);
+            Handles.DrawBezier(_startLine, _endLine, _startLine, _endLine, Color.green, null, _thickness);
+
+            DrawRowsGizmos();
         }
+
+        private void DrawRowsGizmos()
+        {
+            float startRowZ = zFront + paddingFront;
+            float endRowZ = zBack - paddingBack;
+            foreach (float zRow in _centeredAlignRowList)
+            {
+                float zDepth = math.lerp(startRowZ, endRowZ, zRow);
+
+                Vector3 rowStartLine = transform.position;
+                rowStartLine.z = zDepth;
+
+                Vector3 rowEndLine = transform.position;
+                rowEndLine.y = _rowHeight;
+                rowEndLine.z = zDepth;
+
+                Handles.color = Color.blue;
+                Handles.DrawLine(rowStartLine, rowEndLine);
+            }
+        }
+#endif
     }
 }
