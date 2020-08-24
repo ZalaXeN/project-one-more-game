e5800cb8afa3a7c93b0bd2deab9b2dec3f0f9327
ZalaXeN <ZalaXeN@users.noreply.github.com>
2020-08-24 14:50:11 +0700

    Add Slash Swing Effect

---
+++ Assets/Playground/Scripts/Graphic/SpriteAfterImage.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.U2D.Animation;
+
+public class SpriteAfterImage : MonoBehaviour
+{
+    public Material _targetMaterial;
+
+    private List<GameObject> trailParts = new List<GameObject>();
+
+    void Start()
+    {
+        InvokeRepeating("SpawnTrailPart", 0, 0.2f); // replace 0.2f with needed repeatRate
+    }
+
+    void SpawnTrailPart()
+    {
+        GameObject trailPart = new GameObject();
+        SpriteRenderer trailPartRenderer = trailPart.AddComponent<SpriteRenderer>();
+        SpriteRenderer baseRenderer = GetComponent<SpriteRenderer>();
+
+        trailPartRenderer.sprite = baseRenderer.sprite;
+        trailPartRenderer.material = _targetMaterial;
+        trailPart.transform.localScale = transform.lossyScale;
+        trailPart.transform.SetPositionAndRotation(transform.position, transform.rotation);
+        trailPartRenderer.sortingLayerID = baseRenderer.sortingLayerID;
+        trailPartRenderer.sortingOrder = baseRenderer.sortingOrder + 1;
+
+        trailParts.Add(trailPart);
+
+        StartCoroutine(FadeTrailPart(trailPartRenderer));
+        Destroy(trailPart, 0.5f); // replace 0.5f with needed lifeTime
+    }
+
+    IEnumerator FadeTrailPart(SpriteRenderer trailPartRenderer)
+    {
+        Color color = trailPartRenderer.color;
+        color.a -= 0.1f; // replace 0.5f with needed alpha decrement
+        trailPartRenderer.color = color;
+
+        yield return new WaitForEndOfFrame();
+    }
+}
---
+++ Assets/Playground/Scripts/Graphic/SwingEffector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SwingEffector : MonoBehaviour
+{
+    [Range(0,1)]
+    public float swingLevel;
+
+    private Material _swingMaterial;
+
+    private void OnEnable()
+    {
+        if (!_swingMaterial)
+        {
+            _swingMaterial = GetComponent<SpriteRenderer>().material;
+        }
+    }
+
+    private void Update()
+    {
+        _swingMaterial.SetFloat("swing_level", swingLevel);
+    }
+}
