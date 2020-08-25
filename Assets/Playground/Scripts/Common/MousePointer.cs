eb97796411be13d4d921943a667e485480e1bc6d
ZalaXeN <ZalaXeN@users.noreply.github.com>
2020-08-25 16:04:24 +0700

    Test IK Scripting

---
+++ Assets/Playground/Scripts/Common/MousePointer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MousePointer : MonoBehaviour
+{
+    private Vector3 _mousePos;
+
+    private void Update()
+    {
+        _mousePos = Input.mousePosition;
+        _mousePos.z = Camera.main.nearClipPlane - Camera.main.transform.position.z;
+
+        transform.position = Camera.main.ScreenToWorldPoint(_mousePos);
+    }
+}
