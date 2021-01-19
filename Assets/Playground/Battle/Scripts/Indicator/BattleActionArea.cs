3559ab7760d05d357624facfae47918a2ac33535
ZalaXeN <ZalaXeN@users.noreply.github.com>
2021-01-20 03:35:51 +0700

    Battle Indicator Range update

--- Assets/Playground/Battle/Scripts/Indicator/BattleActionArea.cs
+++ Assets/Playground/Battle/Scripts/Indicator/BattleActionArea.cs
@@ -7,12 +7,15 @@ namespace ProjectOneMore.Battle
     [RequireComponent(typeof(Collider), typeof(Rigidbody))]
     public class BattleActionArea : MonoBehaviour
     {
+        public Transform parentTransform;
         public Transform groundTransform;
 
         private Collider _areaCollider;
 
         [SerializeField]
         private List<BattleUnit> _unitInAreaList = new List<BattleUnit>();
+
+        private Vector3 _areaIndicatorScale = Vector3.one;
 
         private void OnEnable()
         {
@@ -58,10 +61,28 @@ namespace ProjectOneMore.Battle
             }
         }
 
+        public Vector3 GetIndicatorScale()
+        {
+            _areaIndicatorScale.x = Mathf.Abs(parentTransform != null ? parentTransform.localScale.x : transform.localScale.x);
+            _areaIndicatorScale.y = 1f;
+            _areaIndicatorScale.z = 1f;
+
+            return _areaIndicatorScale;
+        }
+
         public List<BattleUnit> GetUnitInAreaList()
         {
             RemoveMissingUnitFromList();
             return _unitInAreaList;
+        }
+
+        public Vector2 GetAreaSizeDelta()
+        {
+            if(!_areaCollider)
+                _areaCollider = GetComponent<Collider>();
+
+            if (_areaCollider.GetType() == typeof(BoxCollider)) return (_areaCollider as BoxCollider).size;
+            return _areaCollider.bounds.size;
         }
     }
 }
--- Assets/Playground/Battle/Scripts/Indicator/BattleActionIndicator.cs
+++ Assets/Playground/Battle/Scripts/Indicator/BattleActionIndicator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ProjectOneMore.Battle
 {
@@ -7,18 +8,33 @@ namespace ProjectOneMore.Battle
     // Make real
     public class BattleActionIndicator : MonoBehaviour
     {
-        public Transform targetTransform;
+        public BattleActionArea actionArea;
 
         public GameObject attackRangeIndicator;
 
+        private RectTransform rectTransform;
+
         private void Update()
         {
-            transform.position = targetTransform.position;
-
             UpdateIndicator();
+            UpdateIndicatorStatus();
         }
 
+        [ContextMenu("Update Indicator")]
         void UpdateIndicator()
+        {
+            if (!actionArea || !attackRangeIndicator)
+                return;
+
+            if (!rectTransform)
+                rectTransform = transform as RectTransform;
+
+            transform.position = actionArea.transform.position;
+            transform.localScale = actionArea.GetIndicatorScale();
+            rectTransform.sizeDelta = actionArea.GetAreaSizeDelta();
+        }
+
+        void UpdateIndicatorStatus()
         {
             if (BattleManager.main?.battleState == BattleState.Battle && !attackRangeIndicator.activeInHierarchy)
                 attackRangeIndicator.SetActive(true);
