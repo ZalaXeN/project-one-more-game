d13d456a46c39ceda96e7aabeedf826615c8ef44
ZalaXeN <ZalaXeN@users.noreply.github.com>
2021-03-22 18:34:08 +0700

    Add Message System

---
+++ Assets/Playground/Scripts/MessageSystem/MessageSystem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace ProjectOneMore
+{
+    public enum MessageType
+    {
+        DAMAGED,
+        DEAD,
+        RESPAWN,
+        //Add your user defined message type after
+    }
+
+    public interface IMessageReceiver
+    {
+        void OnReceiveMessage(MessageType type, object sender, object msg);
+    }
+}
