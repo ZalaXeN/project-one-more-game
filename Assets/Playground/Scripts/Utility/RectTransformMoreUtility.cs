b62393a68c513c4a16f15fff76ca03d0205af253
ZalaXeN <ZalaXeN@users.noreply.github.com>
2021-05-04 15:13:38 +0700

    Add RectTransform More Utility

---
+++ Assets/Playground/Scripts/Utility/RectTransformMoreUtility.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ProjectOneMore
+{
+    public static class RectTransformMoreUtility
+    {
+        public static Vector3 WorldToScreenSpace(Vector3 worldPos, Camera cam, RectTransform area)
+        {
+            Vector3 screenPoint = cam.WorldToScreenPoint(worldPos);
+            screenPoint.z = 0;
+
+            Vector2 screenPos;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(area, screenPoint, cam, out screenPos))
+            {
+                return screenPos;
+            }
+
+            return screenPoint;
+        }
+    }
+}
